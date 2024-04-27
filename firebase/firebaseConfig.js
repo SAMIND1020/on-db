15220261fead502be74922b47f6db2dc1e7aee2f
Samuel Angulo <samdelo1020xpro@gmail.com>
2024-04-27 10:50:20 -0500

    Add the dbURL to firebase config | Finish to first deploy v1.0

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -9,6 +9,7 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_APIKEY,
     authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    databaseURL: import.meta.env.VITE_DATABASEURL,
     projectId: import.meta.env.VITE_PROJECTID,
     storageBucket: import.meta.env.VITE_STORAGEBUCKET,
     messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
