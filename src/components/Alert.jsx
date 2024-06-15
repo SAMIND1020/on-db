e2296c9c988a77eecf594135cea0146bab14dfba
SAMIND1020 <samdelo1020xpro@gmail.com>
2024-06-15 10:08:32 -0500

    Add functionality in Alert

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,23 +1,39 @@
 /* eslint-disable react/prop-types */
 import { ALERT_TYPES } from "../../types";
 
-export default function Alert({ alert }) {
+export default function Alert({ alert, setAlert }) {
     return (
         <>
             {Object.values(alert).length != 0 && (
                 <div
                     className={`${
-                        alert.type != ALERT_TYPES.SUCCESS
-                            ? alert.type !== ALERT_TYPES.ALERT
-                                ? "bg-red-700"
-                                : "bg-indigo-700"
-                            : "bg-green-700"
-                    } p-1 text-white rounded-lg text-center mt-2`}
+                        alert.type !== ALERT_TYPES.ERROR
+                            ? alert.type !== ALERT_TYPES.SUCCESS
+                                ? "bg-indigo-700"
+                                : "bg-green-700"
+                            : "bg-red-700"
+                    } p-1 text-white rounded-lg text-center`}
                 >
                     {typeof alert.msg != "string" ? (
                         alert.msg
                     ) : (
                         <p>{alert.msg}</p>
+                    )}
+                    {alert.type === ALERT_TYPES.CONFIRM && (
+                        <div>
+                            <button
+                                className="bg-green-700 text-sm p-1 rounded-md mx-1"
+                                onClick={alert.handleConfirm}
+                            >
+                                Aceptar
+                            </button>
+                            <button
+                                className="bg-red-700 text-sm p-1 rounded-md mx-1"
+                                onClick={() => setAlert({})}
+                            >
+                                Cancelar
+                            </button>
+                        </div>
                     )}
                 </div>
             )}
